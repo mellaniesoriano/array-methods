c0b1b7139fdff8a07992107de28063ecdaa72105
mellaniesoriano <msoriano808@gmail.com>
2017-05-13 19:57:21 -1000

    slice() done

--- arrayMethods.js
+++ arrayMethods.js
@@ -49,3 +49,7 @@ var updatedOrders = orderQueue.concat({takeOut: "sangria"}, {takeOut: "musubi"})
 var sentence = scrambledWords.join(' ');
 
 var myFruits = fruitCollection.join(' + ');
+
+var favoriteFriends = friends.slice(1,3);
+
+var owesMoney = friends.slice(3,6);
