adc4cb835c76309480f5f6214187eb4871ab431c
mellaniesoriano <msoriano808@gmail.com>
2017-05-13 19:38:54 -1000

    push first and last name done

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,8 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+
+var nameArray = [];
+nameArray.push('Mellanie', 'Soriano');
+
+genericNumberArray.push(6,7,8,9,10);
